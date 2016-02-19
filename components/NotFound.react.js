304e74dde40ad1d3bd637e11de0b8d8089080728
ch_sss <sdhhqb@gmail.com>
2016-02-19 16:46:17 +0800

    add notFound view

---
+++ components/NotFound.react.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+var NotFound = React.createClass({
+	render: function () {
+		return (
+			<div>content not found</div>
+		);
+	}
+});
+export default NotFound;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, hashHistory } from 'react-router';
 import MyApp from './components/MyApp.react';
+import NotFound from './components/NotFound.react';
 
 var rootRoute = {
 	component: 'div',
@@ -10,7 +11,11 @@ var rootRoute = {
 		component: MyApp,
 		childRoutes: [
 			require('./routes/Product'),
-			require('./routes/User')
+			require('./routes/User'),
+			{
+				path: '*',
+				component: NotFound
+			}
 		]
 	}]
 };
